d265845182f10d8fa99c7c0883549c270099dea6
Gabriel <gabriel_37410@yahoo.com>
2020-04-19 14:52:37 -0300

    inicio

---
+++ buscaBinaria.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int busca(){
+
+
+
+
+}
+
+void buscaString() {
+    printf("\nBusca String \n");
+
+    float frase;
+    printf("Digite uma frase que deseja buscar: ");
+    scanf("%f", &frase);
+
+    //BUSCA EM STRING
+    busca();
+}
